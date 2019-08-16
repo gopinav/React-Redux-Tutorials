f4894a176f74a40fb91810c1d4db706e46d1c72b
Vishwas <gopinav5@gmail.com>
2019-08-16 15:22:07 +0530

    Reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -6,3 +6,16 @@ function buyCake () {
     info: 'First redux action'
   }
 }
+
+const initialState = {
+  numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE: return {
+      numOfCakes: state.numOfCakes - 1
+    }
+    default: return state
+  }
+}
