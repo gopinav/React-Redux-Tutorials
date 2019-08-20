1afade9b56f90f49fc0be18f32c275cce7b973bc
Vishwas <gopinav5@gmail.com>
2019-08-20 19:54:18 +0530

    Combine reducers

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,7 +1,9 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake () {
   return {
@@ -10,11 +12,40 @@ function buyCake () {
   }
 }
 
-const initialState = {
+function buyIceCream () {
+  return {
+    type: BUY_ICECREAM
+  }
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20
+// }
+
+const initialCakeState = {
   numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numOfIceCreams: 20
+}
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE: return {
+//       ...state,
+//       numOfCakes: state.numOfCakes - 1
+//     }
+//     case BUY_ICECREAM: return {
+//       ...state,
+//       numOfIceCreams: state.numOfIceCreams - 1
+//     }
+//     default: return state
+//   }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state,
@@ -24,10 +55,26 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer)
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM: return {
+      ...state,
+      numOfIceCreams: state.numOfIceCreams - 1
+    }
+    default: return state
+  }
+}
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer)
 console.log('Initial State ', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Updated State ', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
