1e030e67768db9e77bc00fd5697bac6debb369d2
Vishwas <gopinav5@gmail.com>
2019-08-20 17:04:13 +0530

    Spread state in reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -17,6 +17,7 @@ const initialState = {
 const reducer = (state, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
+      ...state,
       numOfCakes: state.numOfCakes - 1
     }
     default: return state
