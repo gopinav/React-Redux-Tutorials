3b5c08a6338283405242fc9b6cc6d7396335b243
Vishwas <gopinav5@gmail.com>
2019-08-22 21:08:31 +0530

    Add redux-logger middleware

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,6 +1,9 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -69,9 +72,9 @@ const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer
 })
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial State ', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated State ', store.getState()))
+const unsubscribe = store.subscribe(() => { })
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
