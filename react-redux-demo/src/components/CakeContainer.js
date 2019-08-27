f4ab4b3052343c62f489a5ef39c72c01cc24b4a3
Vishwas <gopinav5@gmail.com>
2019-08-27 19:43:21 +0530

    Add cake container

---
+++ react-redux-demo/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer () {
+  return (
+    <div>
+      <h2>Number of cakes - </h2>
+      <button>Buy Cake</button>
+    </div>
+  )
+}
+
+export default CakeContainer
