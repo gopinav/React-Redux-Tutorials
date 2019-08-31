c807d4df6fb75914db009711849a7177e8f3f434
Vishwas <gopinav5@gmail.com>
2019-08-31 18:25:35 +0530

    Add reducer

---
+++ react-redux-demo/src/redux/cake/cakeReducer.js
@@ -0,0 +1,18 @@
+import { BUY_CAKE } from './cakeTypes'
+
+const initialState = {
+  numOfCakes: 10
+}
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE: return {
+      ...state,
+      numOfCakes: state.numOfCakes - 1
+    }
+
+    default: return state
+  }
+}
+
+export default cakeReducer
