1af0fd3ef21ffd13a83ef8ffc5857fe3c18da296
Vishwas <gopinav5@gmail.com>
2019-09-21 15:57:24 +0530

    Redux devtools extension

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import logger from 'redux-logger'
 
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+)
 
 export default store
