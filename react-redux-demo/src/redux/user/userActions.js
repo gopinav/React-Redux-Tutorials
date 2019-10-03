fa18887649b0a774245f3d3c94173bebecb0d839
Vishwas <gopinav5@gmail.com>
2019-10-03 12:05:53 +0530

    user Actions

---
+++ react-redux-demo/src/redux/user/userActions.js
@@ -0,0 +1,25 @@
+import {
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+  FETCH_USERS_FAILURE
+} from './userTypes'
+
+export const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+export const fetchUsersSuccess = users => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+export const fetchUsersFailure = error => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  }
+}
